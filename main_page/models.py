673bdacc0f2023cf2bc60569e6e70868ea9a0fa1
AndriiZanik <zanik.andriy@gmail.com>
2022-04-19 17:24:56 +0300

    add models for main page

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    photo = models.ImageField(blank=True,upload_to='photos/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
