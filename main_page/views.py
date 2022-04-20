47e266d6755428f60f62ddd6320da1294e8fd034
AndriiZanik <zanik.andriy@gmail.com>
2022-04-20 14:34:32 +0300

    add templates

--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import News
+from .models import News
 
 def index(request):
     news = News.objects.all()
-    return HttpResponse('ffff')
+    context = {'Title':'Main Page',
+               'news':news}
+    return render(request=request,template_name='main_page/news_on_main_page.html',context=context)
