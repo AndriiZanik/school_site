d9c1d92fad533b952c20758fe2ffe3a88abdc858
AndriiZanik <zanik.andriy@gmail.com>
2022-04-20 13:30:11 +0300

    add aditional setings

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import News
 
 def index(request):
+    news = News.objects.all()
     return HttpResponse('ffff')
--- school_site/urls.py
+++ school_site/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 import main_page.urls
@@ -22,3 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_page.urls), name='main'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
