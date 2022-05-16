6fa5f69e6823061f98ea919b949235970a0cab32
AndriiZanik <zanik.andriy@gmail.com>
2022-05-16 11:58:56 +0300

    add new class for forms

--- contact/urls.py
+++ contact/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', contacts_page),
-    path('add-response/', add_response),
+    path('', ContactForm.as_view()),
+    #path('add-response/', add_response),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from contact.models import Responses
 from contact.forms import ResponseForm
+from django.views.generic import CreateView
 
 
-def contacts_page(request):
-    if request.method == 'POST':
-        form = ResponseForm(request.POST)
-        if form.is_valid():
-            form.save()
-        #return redirect()
-    else:
-        form = ResponseForm()
-    context = {'Title': 'Контакти','form': form}
-    return render(request=request, template_name='Contacts_page.html', context=context)
+class ContactForm(CreateView):
+    form_class = ResponseForm
+    template_name = 'contacts_page.html'
 
 
-def add_response(request):
-    if request.method == 'POST':
-        form = ResponseForm(request.POST)
-        if form.is_valid():
-            Responses.objects.create(**form.cleaned_data)
-            #return redirect()
-    else:
-        form = ResponseForm()
-    return render(request, 'add_response.html', {'form': form})
+#
+# def contacts_page(request):
+#     if request.method == 'POST':
+#         form = ResponseForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         #return redirect()
+#     else:
+#         form = ResponseForm()
+#     context = {'Title': 'Контакти','form': form}
+#     return render(request=request, template_name='contacts_page.html', context=context)
+#
+#
+# def add_response(request):
+#     if request.method == 'POST':
+#         form = ResponseForm(request.POST)
+#         if form.is_valid():
+#             Responses.objects.create(**form.cleaned_data)
+#             #return redirect()
+#     else:
+#         form = ResponseForm()
+#     return render(request, 'add_response.html', {'form': form})
